a34db424d420b62477f142e6c1e4904bd1513436
Mohammad <moeq24@yahoo.com>
2021-07-26 06:29:09 -0500

    practice

---
+++ src/test/java/com/demoblaze/step_definitions/Hooks.java
@@ -0,0 +1,29 @@
+package com.demoblaze.step_definitions;
+import com.demoblaze.utilities.Driver;
+import io.cucumber.java.After;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class Hooks {
+
+    @After
+    public void tearDownScenario(Scenario scenario){
+
+        //IF MY SCENARIO FAILS
+        // TAKE A SCREENSHOT
+
+        //scenario.isFailed() --> if scenario fails : returns true
+
+        if (scenario.isFailed()){
+
+            byte [] screenshot = ((TakesScreenshot) Driver.getDriver()).getScreenshotAs(OutputType.BYTES);
+            scenario.attach(screenshot, "image/png", scenario.getName());
+
+        }
+
+        System.out.println("AFTER--Teardown steps are being applied...");
+        Driver.closeDriver();
+    }
+
+}
--- src/test/java/com/demoblaze/step_definitions/purchaseStepDefs.java
+++ src/test/java/com/demoblaze/step_definitions/purchaseStepDefs.java
@@ -1,12 +1,27 @@
 package com.demoblaze.step_definitions;
 
+import com.demoblaze.pages.AdidasPage;
 import com.demoblaze.utilities.ConfigurationReader;
 import com.demoblaze.utilities.Driver;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
 
 public class purchaseStepDefs {
+
+    AdidasPage adidasPage = new AdidasPage();
+    int expectedPurchaseAmount = 0;
+
     @Given("User is on the Home Page")
     public void user_is_on_the_home_page() {
+
         Driver.getDriver().get(ConfigurationReader.getProperty("url"));
+
+    }
+
+
+    @When("User adds {string} from {string}")
+    public void user_adds_from(String product, String category) {
+        expectedPurchaseAmount += adidasPage.productAdder(category, product);
+        System.out.println("expectedPurchaseAmount = " + expectedPurchaseAmount);
     }
 }
